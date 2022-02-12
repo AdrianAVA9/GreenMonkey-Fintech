51eaf9175b9a604453222a94d85f55e19adeeb9a
Adrián Vega <avegaa@ucenfotec.ac.cr>
2022-02-12 11:00:35 -0600

    Memory persistence

---
+++ GreenMonkey/GreenMonkey.DataAccess/MemoryPersistence.cs
@@ -0,0 +1,22 @@
+﻿using GreenMonkey.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenMonkey.DataAccess
+{
+    public static class MemoryPersistence
+    {
+        public static Dictionary<string, object> Persistency = new Dictionary<string, object>();
+        public static void StoreData(string code, object data)
+        {
+            Persistency[code] = data;
+        }
+        public static Dictionary<string, object> GetData()
+        {
+            return Persistency;
+        }
+    }
+}
