544c88773ff19c5703e933c5d7c218cfd0bee016
Adrián Vega <avegaa@ucenfotec.ac.cr>
2022-02-17 20:53:37 -0600

    Adding script of suscriptor table

---
+++ GreenMonkey/GreenMonkey.DataAccess/ScriptsDB/db.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[TBL_Suscriptor](
+	[Code] [nvarchar](4) NOT NULL PRIMARY KEY,
+	[Name] [nvarchar](50) NOT NULL,
+	[Description] [nvarchar](250) NOT NULL,
+	[Owner] [nvarchar](50) NOT NULL,
+	[BaseUrl] [nvarchar](50) NOT NULL
+)
