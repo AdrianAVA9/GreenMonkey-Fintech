88618f63ff2a645026db98259ea2f8c6277654c0
Adrián Vega <avegaa@ucenfotec.ac.cr>
2022-02-19 12:54:47 -0600

    Store procedure to create suscriptor

--- GreenMonkey/GreenMonkey.DataAccess/ScriptsDB/dbStoreProcedure.sql
+++ GreenMonkey/GreenMonkey.DataAccess/ScriptsDB/dbStoreProcedure.sql
@@ -1,1 +1,15 @@
-﻿
+﻿CREATE PROCEDURE CRE_SUSCRIPTOR_PR(
+	@CODE nvarchar(4),
+	@NAME nvarchar(50),
+	@DESCRIPTION nvarchar(250),
+	@OWNER nvarchar(50),
+	@BASE_URL nvarchar(50)
+)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	INSERT INTO TBL_Suscriptor(Code, Name, Description, Owner, BaseUrl, Active)
+	VALUES
+		(@CODE, @NAME, @DESCRIPTION, @OWNER, @BASE_URL, 'A')
+END
