80adfb3654363fdf79491d92dc69670423cf9f6a
Adrián Vega <avegaa@ucenfotec.ac.cr>
2022-02-19 12:36:39 -0600

    Adding new property

--- GreenMonkey/GreenMonkey.DataAccess/ScriptsDB/db.sql
+++ GreenMonkey/GreenMonkey.DataAccess/ScriptsDB/db.sql
@@ -3,5 +3,6 @@
 	[Name] [nvarchar](50) NOT NULL,
 	[Description] [nvarchar](250) NOT NULL,
 	[Owner] [nvarchar](50) NOT NULL,
-	[BaseUrl] [nvarchar](50) NOT NULL
+	[BaseUrl] [nvarchar](50) NOT NULL,
+	[Active] [char](1) NOT NULL DEFAULT('S'),
 )
