e92f256588c390c68be8bd4984ba765e7aafd7dd
Adrián Vega <avegaa@ucenfotec.ac.cr>
2022-02-19 12:39:30 -0600

    Renaming home to suscriptor

--- GreenMonkey/GreenMonkey.UI/Controllers/HomeController.cs
+++ GreenMonkey/GreenMonkey.UI/Controllers/HomeController.cs
@@ -6,24 +6,10 @@ using System.Web.Mvc;
 
 namespace GreenMonkey.UI.Controllers
 {
-    public class HomeController : Controller
+    public class SuscriptorController : Controller
     {
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
