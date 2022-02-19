1004af52521dc8d5eda089f0e84a1cdd6789515f
Adrián Vega <avegaa@ucenfotec.ac.cr>
2022-02-19 12:38:35 -0600

    Web UI

---
+++ GreenMonkey/GreenMonkey.UI/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace GreenMonkey.UI
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
