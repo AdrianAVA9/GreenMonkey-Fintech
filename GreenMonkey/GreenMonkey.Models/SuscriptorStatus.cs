d962cc187d8b0ed948dec5790640d3b89c75b8cc
Adrián Vega <avegaa@ucenfotec.ac.cr>
2022-02-24 18:42:38 -0600

    Adding Suscriptor status abstraction

---
+++ GreenMonkey/GreenMonkey.Models/SuscriptorStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenMonkey.Models
+{
+    public class SuscriptorStatus
+    {
+        public string Status { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
