d36c02990a5c671391d5c0d960fe54aff1dc62fd
Adrián Vega <avegaa@ucenfotec.ac.cr>
2022-03-03 17:45:24 -0600

    Transaction validator

---
+++ GreenMonkey/GreenMonkey.Api/Validators/TransactionValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using GreenMonkey.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GreenMonkey.Api.Validators
+{
+    public class TransactionValidator : AbstractValidator<TransactionDto>
+    {
+        public TransactionValidator()
+        {
+            RuleFor(x => x.Amount)
+              .NotEmpty()
+              .GreaterThan(0);
+
+            RuleFor(x => x.Type)
+                .NotEmpty()
+                .Must(x => x == "debe" || x == "haber")
+                .WithMessage("Invalid type");
+
+            RuleFor(x => x.AccountNumber)
+               .NotEmpty()
+               .Length(14, 14);
+        }
+    }
+}
