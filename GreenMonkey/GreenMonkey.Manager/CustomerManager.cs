60321979fb1f44bfb1683eb4c655553c4722ab2d
Adrián Vega <avegaa@ucenfotec.ac.cr>
2022-03-10 18:22:36 -0600

    Send SMS message

--- GreenMonkey/GreenMonkey.Manager/CustomerManager.cs
+++ GreenMonkey/GreenMonkey.Manager/CustomerManager.cs
@@ -1,5 +1,6 @@
 ﻿using GreenMonkey.DataAccess.Crud;
 using GreenMonkey.Models;
+using GreenMonkey.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,6 +26,8 @@ namespace GreenMonkey.Manager
 
         public Customer RetrieveCustomer(Customer customer)
         {
+            var smsClient = new SMSClient("092a341683abe9326d9866e622d907e8", "ACf01d579cfb2428dd04e5e6962b97ab7c", "+15402992669");
+            smsClient.SendMessage("Hello World", "+50671221940");
             return CustomerFactory.Retrieve<Customer>(customer);
         }
 
---
+++ GreenMonkey/GreenMonkey.Utils/SMSClient.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace GreenMonkey.Utils
+{
+    public class SMSClient
+    {
+        public string Token { get; set; }
+        public string AccountId { get; set; }
+        public string PhoneNumber { get; set; }
+        public SMSClient(string token, string accountId, string phoneNumber)
+        {
+            Token = token;
+            AccountId = accountId;
+            PhoneNumber = phoneNumber;
+        }
+
+        public void SendMessage(string message, string phoneNumber)
+        {
+            TwilioClient.Init(AccountId, Token);
+            var messageResource = MessageResource.Create(
+                body: message,
+                from: new PhoneNumber(PhoneNumber),
+                to: new PhoneNumber(phoneNumber)
+              );
+
+            var result = messageResource.DateSent;
+        }
+    }
+}
